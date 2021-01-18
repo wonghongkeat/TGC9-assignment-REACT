c5ee323edb162423060693134a8b5f0f90f6d8a0
wonghongkeat <hongkeat1991@gmail.com>
2021-01-18 08:31:35 +0000

    edited:
    added bootstrap navbar

--- assignment3/src/Frontpage.js
+++ assignment3/src/Frontpage.js
@@ -1,11 +1,28 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Frontpage(){
     return (
         <React.Fragment>
-            <div>
-                <h1>hello world</h1>
-            </div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Suck It</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">menu <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">example</a>
+      </li>
+    </ul>
+  </div>
+</nav>
         </React.Fragment>
     )
 }
