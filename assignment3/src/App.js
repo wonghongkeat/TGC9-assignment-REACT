a40d791c73a7876abc72a23ee7652e7842a6eaf7
wonghongkeat <hongkeat1991@gmail.com>
2021-01-18 07:57:15 +0000

    edited:
    first commit: Set up react

---
+++ assignment3/src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Frontpage from "./Frontpage"
+
+function App() {
+  return (
+    <div className="App">
+        <Frontpage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ assignment3/src/Frontpage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Frontpage(){
+    return (
+        <React.Fragment>
+            <div>
+                <h1>hello world</h1>
+            </div>
+        </React.Fragment>
+    )
+}
