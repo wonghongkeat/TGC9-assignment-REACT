e485fb410a17ec7f7e0399e7b2b3784bcd948ed9
wonghongkeat <hongkeat1991@gmail.com>
2021-01-22 03:52:44 +0000

    edited:
    added colors to navbar

--- assignment3/src/Frontpage.js
+++ assignment3/src/Frontpage.js
@@ -1,24 +1,25 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './frontPage.css'
 
 export default function Frontpage(){
     return (
         <React.Fragment>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Suck It</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+  <nav className="navbar navbar-expand-lg">
+  <a className="navbar-brand" href="#">Suck It</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">menu <span class="sr-only">(current)</span></a>
+  <div className="collapse navbar-collapse" id="navbarNav">
+    <ul className="navbar-nav ml-auto" >
+      <li className="nav-item active">
+        <a className="nav-link" href="#">menu <span className="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Login</a>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Login</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">example</a>
+      <li className="nav-item">
+        <a className="nav-link" href="#">cart</a>
       </li>
     </ul>
   </div>
