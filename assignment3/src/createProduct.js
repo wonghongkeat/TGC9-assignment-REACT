0b5f01bb327263159949c3b353b58d7c04613467
wonghongkeat <hongkeat1991@gmail.com>
2021-02-03 08:56:40 +0000

    edited:
    edited code and able to push to new product from react

--- assignment3/src/createProduct.js
+++ assignment3/src/createProduct.js
@@ -6,14 +6,16 @@ const base_url = "https://8080-c693cb85-d658-493b-9a4c-ec22f40a36b6.ws-us03.gitp
 export default class createProduct extends React.Component {
 
     state = {
-        flavours:[],
-        toppings:[],
-        sugars:[],
-       
+        flavours: [],
+        toppings: [],
+        sugars: [],
+            flavour: "",
+            topping: [],
+            sugar: ""
 
     }
 
-       async componentDidMount() {
+    async componentDidMount() {
         let responseTopping = await axios.get(`${base_url}reactTopping`)
         let responseFlavour = await axios.get(`${base_url}reactFlavours`)
         let responseSugar = await axios.get(`${base_url}reactSugar`)
@@ -28,57 +30,95 @@ export default class createProduct extends React.Component {
         return (
             <div>
                 <div>
-                  <h3>flavour</h3>
-                    {this.state.flavours.map(f=>(
+                    <h3>flavour</h3>
+                    {this.state.flavours.map(f => (
                         <React.Fragment>
-                    <input 
-                    type="radio"
-                    value={f.id}
-                    name="flavour"
-                    onChange={this.updateFormField}
-                    />
-                    <label>{f.tea}</label>
-                    </React.Fragment>
+                            <input
+                                type="radio"
+                                value={f.id}
+                                name="flavour"
+                                onChange={this.updateFlavourField}
+                            />
+                            <label>{f.tea}</label>
+                        </React.Fragment>
                     ))}
                 </div>
                 <div>
-                  <h3>sugar level</h3>
-                    {this.state.sugars.map(s=>(
-                    <React.Fragment>
-                    <input 
-                    type="radio"
-                    value={s.id}
-                    name="sugar"
-                    onChange={this.updateFormField}
-                    />
-                    <label>{s.level}</label>
-                    </React.Fragment>
+                    <h3>sugar level</h3>
+                    {this.state.sugars.map(s => (
+                        <React.Fragment>
+                            <input
+                                type="radio"
+                                value={s.id}
+                                name="sugar"
+                                onChange={this.updateSugarField}
+                            />
+                            <label>{s.level}</label>
+                        </React.Fragment>
                     ))}
                 </div>
                 <div>
-                  <h3>topping</h3>
-                    {this.state.toppings.map(t=>(
-                    <React.Fragment>
-                    <input 
-                    type="checkbox"
-                    value={t.id}
-                    name="topping"
-                    onChange={this.updateFormField}
-                    />
-                    <label>{t.topping}</label>
-                    </React.Fragment>
+                    <h3>topping</h3>
+                    {this.state.toppings.map(t => (
+                        <React.Fragment>
+                            <input
+                                type="checkbox"
+                                value={t.id}
+                                name="topping"
+                                onChange={this.updateToppingField}
+                            />
+                            <label>{t.topping}</label>
+                        </React.Fragment>
                     ))}
                 </div>
-</div>
-           
-
-
+                <button onClick={this.create}>add to cart</button>
+            </div>
         )
     }
 
-      updateFormField = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  };
+    updateFlavourField = (e) => {
+        this.setState({
+        flavour: e.target.value
+        });
+    };
+     updateSugarField = (e) => {
+        this.setState({
+        sugar: e.target.value
+        });
+        
+    };
+    //   updateToppingField = (e) => {
+    //     this.setState({
+    //     topping: e.target.value
+    //     });
+    //     console.log(this.state.sugar,this.state.topping)
+    // };
+
+    updateToppingField = (e) => {
+    let originalTopping = this.state[e.target.name];
+    if (!originalTopping.includes(e.target.value)) {
+        let modified = [...originalTopping, e.target.value];
+        this.setState({
+            [e.target.name] : modified
+        })
+    } else {
+        let modified = originalTopping.filter((eachItem)=>{
+            return eachItem !== e.target.value;
+        })
+        this.setState({
+            [e.target.name] : modified
+        })
+    }
+    console.log(this.state.topping)
+  }
+   
+
+    create = async () =>{
+        await axios.post(`${base_url}react/create`,{
+            flavour: this.state.flavour,
+            sugar: this.state.sugar,
+            topping: this.state.topping
+        }
+        )
+    }
 }
